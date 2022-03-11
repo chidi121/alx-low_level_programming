c150fc17d335991102ffe8f4e02b68b15f1e2355
Blessing Odo <blessingodo25@gmail.com>
2022-03-11 15:39:57 -0800

    My comb commit

--- 0x01-variables_if_else_while/9-print_comb.c
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -11,15 +11,16 @@ int main(void)
 {
 	int i;
 
-	for (i = 48; i < 58; i++)
+	for (i = 0; i <= 9;  i++)
 	{
-		putchar(i);
-	}
-	if (i != 57)
-	{
+		putchar((i % 10) + '0');
+		if (i == 9)
+			continue;
+	
 		putchar(',');
 		putchar(' ');
 	}
+
 	putchar('\n');
 
 	return (0);
