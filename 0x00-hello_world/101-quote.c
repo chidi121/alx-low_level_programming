05281bf87b099dff1e0f1ae1fcbf414735af0d34
Blessing Odo <blessingodo25@gmail.com>
2022-03-10 17:02:15 -0800

    My quote commit

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,13 +1,15 @@
 #include <stdio.h>
 #include <unistd.h>
 /**
- * main - Entry point
+ * main - prints "and that piece of art is useful" - Dora Korpar, 2015-10-19",
+ *                followed by a new line, to standard error.
  *
  * Return: Always 1 (Success)
  */
 int main(void)
 {
-	char ch[] = ("and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 59);
-	write(2, ch, sizeof(ch));
+	write(2,
+	      "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n",
+	      59);
 	return (1);
 }
