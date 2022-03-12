5d9800445f21d60b8bb3d5ac7b9bd8345791fa3f
Blessing Odo <blessingodo25@gmail.com>
2022-03-11 16:40:37 -0800

    My comb5 commit

--- 0x01-variables_if_else_while/102-print_comb5.c
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -12,7 +12,7 @@ int main(void)
 
 	for (i = 0; i <= 98; i++)
 	{
-		for j = i +1; j <= 99; j++)
+		for j = i + 1; j <= 99; j++)
 		{
 			putchar((i / 10) + '0');
 			putchar((i % 10) + '0');
@@ -31,4 +31,4 @@ int main(void)
 	putchar('\n');
 
 	return (0);
-
+}
