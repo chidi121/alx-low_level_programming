c7adb80e5a5b6fdb9017d2e764adf44718ce34a3
Blessing Odo <blessingodo25@gmail.com>
2022-03-15 15:35:32 -0700

    My alpha commit

--- 0x02-functions_nested_loops/4-isalpha.c
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -2,13 +2,12 @@
 
 /**
  * _isalpha - functions that checks for alphabetic character
- *
+ * @c: single letter input
  * Return: 1 if c is a letter, lowercase or uppercase, Return 0 otherwise
  */
 
 int _islower(int c)
 {
-	int c;
 
 	if (((c >= 'a') && (c <= 'z')) || ((c >= 'A') (c <= 'Z')))
 		return (1);
