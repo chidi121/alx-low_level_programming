19fdbd019513a19b4cb16ce77c09ceedbbe1d731
Blessing Odo <blessingodo25@gmail.com>
2022-03-15 15:19:41 -0700

    My alpha4 commit

---
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/**
+ * _isalpha - functions that checks for alphabetic character
+ *
+ * Return: 1 if c is a letter, lowercase or uppercase, Return 0 otherwise
+ */
+
+int _islower(int c)
+{
+	if (((c >= 'a') && (c <= 'z')) || ((c >= 'A') (c <= 'Z')))
+		return (1);
+	else
+		return (0);
+}
