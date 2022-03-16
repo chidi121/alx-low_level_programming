5df8369cab4690ab0168cd3c18afe9f46f6491d6
Blessing Odo <blessingodo25@gmail.com>
2022-03-16 16:18:41 -0700

    my commit

--- 0x02-functions_nested_loops/102-fibonacci.c
+++ 0x02-functions_nested_loops/102-fibonacci.c
@@ -2,7 +2,7 @@
 
 /**
  * main - main block
- * description: computes and printsthe sum of all the multiples of 3 or 
+ * description: computes and printsthe sum of all the multiples of 3 or
  * 5 below 1024 (excluded), followed by a new line
  * return: 0
  */
