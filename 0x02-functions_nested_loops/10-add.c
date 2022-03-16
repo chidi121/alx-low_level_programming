af754bd0ee4d51c5e31eeb969506a00efa382af7
Blessing Odo <blessingodo25@gmail.com>
2022-03-15 17:58:11 -0700

    add commit

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,19 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * add - Sums two integer and
+ * return the result.
+ *
+ * @num1: interger one.
+ * @num2: interger two.
+ *
+ * Return: Sum of the two integers
+ */
+
+int add(int num1, int num2)
+{
+	int sum = num1 + num2;
+
+	return (sum);
+}
