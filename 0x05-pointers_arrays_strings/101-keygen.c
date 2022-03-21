fb516edb6f623650ab6859e56e85f3df374ef109
Blessing Odd <blessingodo25@gmail.com>
2022-03-21 14:16:03 -0700

    keygen commit

--- 0x05-pointers_arrays_strings/101-keygen.c
+++ 0x05-pointers_arrays_strings/101-keygen.c
@@ -1,15 +1,33 @@
-#include "main.h"
+#include <stdio.h>
 #include <stdlib.h>
 #include <time.h>
-#include <stdio.h>
 /**
+ *main - generates random passwords
+ *
+ *Return:0(success)
+ *
  */
 int main(void)
 {
-int num;
+	int i, sum, n;
+	int pass[100];
 
-srand(time(0));
-num = rand();
-printf("Anything; %i\n", num);
-return (0);
+	sum = 0;
+
+	srand(time(NULL));
+
+	for (i = 0; i < 100; i++)
+	{
+		pass[i] = rand() % 78;
+		sum += (pass[i] + '0');
+		putchar(pass[i] + '0');
+		if ((2772 - sum) - '0' < 78)
+		{
+			n = 2772 - sum - '0';
+			sum += n;
+			putchar(n + '0');
+			break;
+		}
+	}
+	return (0);
 }
