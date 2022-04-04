f25aa97ad3ada91cc9e2e8ef73329c275d5cd450
Blessing Odd <blessingodo25@gmail.com>
2022-04-04 08:20:39 -0700

    args

---
+++ 0x0A-argc_argv/1-args.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+/**
+ * main -  writes its name
+ * @argc: arguement count
+ * @argv: array of pointers to arguement strings
+ * Return: 0
+ **/
+
+int main(int argc, char *argv[])
+{
+	int i = 0;
+
+	argv = argv;
+
+	if (i < argc)
+		{
+		printf("%d\n", argc - 1);
+		}
+	return (0);
+}
